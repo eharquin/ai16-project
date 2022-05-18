27198e35984ec01b5739aa960f493e598f124d97
Antonin <antonin.guyot@icloud.com>
2022-05-18 08:30:07 +0200

    Initial commit

---
+++ src/main/java/fr/utc/communicator/Communicator2022Application.java
@@ -0,0 +1,13 @@
+package fr.utc.communicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Communicator2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Communicator2022Application.class, args);
+    }
+
+}
