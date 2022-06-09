21c44103a752a851c35671753165df217489bea8
Matthieu SOLAN <matthieu.solan@etu.utc.fr>
2022-06-09 11:27:58 +0200

    page admin

--- src/main/java/fr/utc/communicator/controllers/AdminController.java
+++ src/main/java/fr/utc/communicator/controllers/AdminController.java
@@ -1,8 +1,20 @@
 package fr.utc.communicator.controllers;
 
+import fr.utc.communicator.models.User;
+import fr.utc.communicator.messages.MessageSent;
+import fr.utc.communicator.models.Channel;
+import fr.utc.communicator.repositories.ChannelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+import fr.utc.communicator.repositories.UserRepository;
+
+import java.security.Principal;
+import java.util.List;
 
 
 @Controller
@@ -14,6 +26,15 @@ public class AdminController {
     public String login() {
         return "admin";
     }
+
+    @SendTo("/admin")
+    public List<User> sendUserList() throws Exception {
+        List<User> users = userRepository.findAll();
+        return users;
+    }
+
 }
+
+
 
 
